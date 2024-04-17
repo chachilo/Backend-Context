944e5d2c4b15df04e5c3f952cd9448dd9ff2955c
Rafael Galindo F <126412980+chachilo@users.noreply.github.com>
2024-04-16 18:36:42 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGO_USERNAME = "rafael3041220219";
-const MONGO_PASSWORD = "Casterfiler25";
+const MONGO_PASSWORD = "Casterfiler25%";
 const MONGO_CLUSTER = "cluster0.m9fchhq.mongodb.net";
 const MONGO_DATABASE = "usuarios"; // Reemplaza "usuarios" con el nombre de tu base de datos
 
