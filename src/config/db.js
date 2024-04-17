02aeb38579161cef7e140cbac2c3e99084bbf12a
Rafael Galindo F <126412980+chachilo@users.noreply.github.com>
2024-04-16 18:34:22 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb+srv://rafael3041220219:Casterfiler25%@cluster0.m9fchhq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_USERNAME = "rafael3041220219";
+const MONGO_PASSWORD = "Casterfiler25";
+const MONGO_CLUSTER = "cluster0.m9fchhq.mongodb.net";
+const MONGO_DATABASE = "usuarios"; // Reemplaza "usuarios" con el nombre de tu base de datos
+
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DATABASE}`;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
-    console.log("connect mongoDB");
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Conexión exitosa a MongoDB");
   } catch (err) {
-    console.log(err);
+    console.error("Error al conectar a MongoDB:", err);
   }
 };
